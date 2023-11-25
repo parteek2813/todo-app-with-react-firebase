d78e64d5d00bec91cf91a52c45bb15f1cef731fd
parteek kumar <digitalparteek@gmail.com>
2023-11-25 13:43:21 +0530

    calender Component

--- src/Components/Calender.js
+++ src/Components/Calender.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { CalendarDate, CaretUp } from "react-bootstrap-icons";
+import { calendarItems } from "../constants";
 
-function Calender() {
-  return <div className="Calender">Calender</div>;
+function Calendar() {
+  return (
+    <div className="Calendar">
+      <div className="header">
+        <div className="title">
+          <CalendarDate size="18" />
+          <p>Calendar</p>
+        </div>
+        <div className="btns">
+          <span>
+            <CaretUp size="20" />
+          </span>
+        </div>
+      </div>
+      {/* Map over each calender item */}
+      <div className="items">
+        {calendarItems.map((item) => (
+          <div className="item" key={item}>
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Calender;
+export default Calendar;
